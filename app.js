87c3bf56594e24e1b73e9ac000a170f3ca375228
Auraofdivinity <peirisasel373@gmail.com>
2020-02-19 00:29:08 +0530

    Final

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+var Feed = require("rss-to-json");
+
+app.use(cors());
+
+app.get("/posts", function(req, res) {
+  Feed.load("ttps://medium.com/feed/codeuino", function(err, rss) {
+    res.send(rss);
+  });
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("app.listening on port 5000");
+});
