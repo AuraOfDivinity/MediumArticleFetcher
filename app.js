6495241460c92ea822958313d0154c4c4428b789
Auraofdivinity <peirisasel373@gmail.com>
2020-02-19 01:00:43 +0530

    Final

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const cors = require("cors");
 const app = express();
 var Feed = require("rss-to-json");
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.get("/posts", function(req, res) {
   Feed.load("https://medium.com/feed/codeuino", function(err, rss) {
